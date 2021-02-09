d700ab03679e247e6552840d65ec4b521b6e9f4a
tfdsimoes <tfdsimoes@gmail.com>
2021-02-09 10:51:28 +0100

    Add singleton for in-memory storage

---
+++ src/main/java/com/sportradar/scoreboard/repository/GameRepository.java
@@ -0,0 +1,41 @@
+package com.sportradar.scoreboard.repository;
+
+import com.sportradar.scoreboard.entity.Game;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+public class GameRepository {
+
+    private static GameRepository gameRepository = null;
+
+    private Map<String, Game> scoreBoard = new HashMap<>();
+
+    public static GameRepository getInstance() {
+        if(gameRepository == null) {
+            gameRepository = new GameRepository();
+        }
+        return gameRepository;
+    }
+
+    public void add(Game game) {
+        scoreBoard.put(game.getId(), game);
+    }
+
+    public Game get(String id) {
+        return scoreBoard.get(id);
+    }
+
+    public void remove(String id) {
+        scoreBoard.remove(id);
+    }
+
+    public void update(Game game) {
+        scoreBoard.put(game.getId(), game);
+    }
+
+    public Collection<Game> getAll() {
+        return scoreBoard.values();
+    }
+}
